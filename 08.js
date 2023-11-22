3f90e7f5134dfa687c5724900b9ddb92c97ffeb2
DmitryChues <dmitrychues2312@gmail.com>
2023-11-22 21:49:25 +0300

    08-11

---
+++ 08.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+	if (name[0] === 'R' || name[0] === 'r') {
+		name = name + ' plays banjo';
+	} else {
+		name = name + ' does not play banjo';
+	}
+	return name;
+}
